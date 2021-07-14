13e17738f0c77fd7caa1bd9272ae896f8160cf8a
Anusha Silla <anushasilla@Anushas-MacBook-Pro-2.local>
2021-07-13 17:53:55 -0700

    created landing page

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+
+def landing(request):
+    return render(request, 'coloring/landing.html')
