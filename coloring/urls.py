4d7d3caa60cb5896b96991586f804a42c004f0c6
Anthony Shafik <anthonyshafik@berkeley.edu>
2021-07-14 21:20:47 -0700

    made some changes, added pen in demo

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('demo', views.demo, name='demo'),
     path('new_interaction', views.index, name='new_interaction'),
-    path('landing', views.landing, name='landing')
+    path('landing', views.landing, name='landing'),
+    path('triangle', views.triangle, name='triangle'),
+    path('flowers', views.flowers, name='flowers')
     
 ]
